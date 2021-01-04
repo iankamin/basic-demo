64a1dd0efb7810c14f302c0602bed741807954f3
iankamin <kamineri.ik@gmail.com>
2021-01-03 19:15:34 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showNumber(0)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # . . # .
+    # . # . .
+    # # . . .
+    # . . . .
+    `)
+basic.pause(100)
+basic.showIcon(IconNames.Heart)
+basic.pause(100)
+basic.showString("Hello!")
